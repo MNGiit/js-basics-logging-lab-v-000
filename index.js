449dd16a9239555f0db1a197096b892a1747bd8f
Mohammed Noori <kmanoorib@gmail.com>
2019-02-20 04:06:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const driver = 'bob';
 
+console.log('this code was called');
 
+console.log(driver, 'is the driver variable name');
 
-console.log()
-invokes console.log() with the string "this code was called" ‣
-AssertionError: expected false to be true
-    at Context.<anonymous> (test/indexTest.js:18:66)expect(spy.calledWithExactly('this code was called')).to.be.true;
-invokes console.log() with the `driver` variable ‣
-AssertionError: expected false to be true
-    at Context.<anonymous> (test/indexTest.js:22:49)expect(spy.calledWithExactly('bob')).to.be.true;
-invokes the console.log with the `driver` variable as the first argument and `is the driver variable name` as the second argument ‣
-AssertionError: expected false to be true
-    at Context.<anonymous> (test/indexTest.js:26:80)expect(spy.calledWithExactly('bob', 'is the driver variable name')).to.be.true;
+console.log('bob', 'is the driver variable name');
+
